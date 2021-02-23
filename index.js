3874ef787a7d8d275794e5a443a849cd28216dba
github username <sharnellgrant@gmail.com>
2021-02-22 23:48:52 -0500

    commit tree ba59dcc83bda36d4434dd8d94104571f5f67b567

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+// function submitData(name, email) {
+//     fetch("https://localhost:3000/users", {
+//         method: "POST",
+//         headers:{ 
+//             "Content-Type":"applicatio/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name,
+//             email
+//         })
+//     })
+//     .then(function(response) {
+//         return response.json()
+//       })
+//       .then(function(object) {
+//         console.log(object)
+//       })
+// }
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+          console.log(object)
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
